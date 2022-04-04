d66cc0a0327ac9c5fbeb4a81994b15fe8fbef031
rashad porter <rashad.porter18@gmail.com>
2022-04-04 01:48:08 -0400

    Added grid and new components to create

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React, { useState } from 'react';
-import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
-import AddEntryForm from './Components/addEntry/AddEntryForm';
-import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
-
+import React, { useState } from "react";
+import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
+import AddEntryForm from "./Components/addEntry/AddEntryForm";
+import EntriesChartTracker from "./Components/EntriesChartTracker/EntriesChartTracker";
+import "./App.css";
 function App() {
+  const [entries, setEntries] = useState([
+    { weight: 175, date: "11-23-2021" },
+    { weight: 176, date: "11-23-2021" },
+  ]);
 
-  const [entries, setEntries] = useState([{weight:175, date: '11-23-2021'},{weight:176, date: '11-23-2021'}])
-  
-  function addNewEntry(entry){
+  function addNewEntry(entry) {
     let tempEntries = [...entries, entry];
 
-    setEntries(tempEntries)
+    setEntries(tempEntries);
   }
- 
- 
+
   return (
-    <div>
-      <DisplayEntries parentEntries = {entries} />
-      <AddEntryForm addNewEntryProperty= {addNewEntry} />
-      <EntriesChartTracker parentEntries= {entries}/>
+    <div className="container-fluid">
+      <div className="row">
+        <h3 style={{margin: '1em'}}>Weight
+        <small className= "text-muted">Tracker</small></h3>
+        <div className="col-md-6">
+          <div className="border-box">
+            <DisplayEntries parentEntries={entries} />
+          </div>
+          <div className="border-box">
+            <AddEntryForm addNewEntryProperty={addNewEntry} />
+          </div>
+        </div>
+        <div className="col-md-6">
+          <EntriesChartTracker parentEntries={entries} />
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/addEntry/AddEntryForm.jsx
+++ src/Components/addEntry/AddEntryForm.jsx
@@ -1,31 +1,49 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import "./AddEntryForm.css";
 
 const AddEntryForm = (props) => {
+  const [weight, setWeight] = useState(0);
+  const [date, setDate] = useState("");
 
-    const [weight, setWeight] = useState(0);
-    const [date, setDate] = useState('');
+  function handleSubmit(event) {
+    event.preventDefault();
+    let newEntry = {
+      weight: weight,
+      date: date,
+    };
+    console.log(newEntry);
+    props.addNewEntryProperty(newEntry);
+  }
 
+  return (
+    <form onSubmit={handleSubmit} className="form-grid">
+      <div className="form-group">
+        <label>Weight</label>
+        <input
+          type="number"
+          class="form-control"
+          value={weight}
+          onChange={(event) => setWeight(parseFloat(event.target.value))}
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="date"
+          className="form-control"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+        />
+        <label>Date</label>
+      </div>
+      <button
+        type="submit"
+        className="btn btn-primary"
+        style={{ "margin-top": "1em" }}
+      >
+        Add
+      </button>
+    </form>
+  );
+};
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        let newEntry ={
-            weight: weight,
-            date: date
-        };
-        console.log(newEntry)
-        props.addNewEntryProperty(newEntry)
-    }
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>Weight</label>
-            <input type='number' value={weight} onChange= {(event) => setWeight(parseFloat(event.target.value))}/>
-            <label>Date</label>
-            <input type='date' value={date} onChange= {(event) => setDate(event.target.value)}/>
-            <button type= 'submit'>Add</button>
-        </form>
-      );
-}
- 
 export default AddEntryForm;
